5295bbe640c8331359fc7417f7f993465638a471
dreak603 <dreak603@gmail.com>
2019-10-28 15:49:22 +0800

    add signup fn

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 const db = require('./models');
 const app = express();
 const port = 3000;
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+  require('dotenv').config();
 }
 
 // 設定 view engine 使用 handlebars
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(port, () => {
-    db.sequelize.sync();
-    console.log(`Example app listening on port http://localhost:${port}`);
+  db.sequelize.sync();
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
 
 require('./routes')(app);
--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING
+    },
+    {}
+  );
   User.associate = function(models) {
     // associations can be defined here
   };
