9d7079a35a478571c22baac196eed6f261964896
dreak603 <dreak603@gmail.com>
2019-10-28 19:26:48 +0800

    add auth

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+  require('dotenv').config();
 }
 
 // 設定 view engine 使用 handlebars
@@ -17,18 +17,20 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(passport.initialize());
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 app.use((req, res, next) => {
-    res.locals.success_messages = req.flash('success_messages');
-    res.locals.error_messages = req.flash('error_messages');
-    next();
+  res.locals.success_messages = req.flash('success_messages');
+  res.locals.error_messages = req.flash('error_messages');
+  res.locals.user = req.user;
+  next();
 });
 
 app.listen(port, () => {
-    db.sequelize.sync();
-    console.log(`Example app listening on port http://localhost:${port}`);
+  db.sequelize.sync();
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
 
-require('./routes')(app);
+require('./routes')(app, passport);
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,50 @@
 const restController = require('../controllers/restController.js');
 const adminController = require('../controllers/adminController.js');
 const userController = require('../controllers/userController.js');
-const passport = require('passport');
 
-module.exports = app => {
-    //如果使用者訪問首頁，就導向 /restaurants 的頁面
-    app.get('/', (req, res) => res.redirect('/restaurants'));
+module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/signin');
+  };
 
-    //在 /restaurants 底下則交給 restController.getRestaurants 來處理
-    app.get('/restaurants', restController.getRestaurants);
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next();
+      }
+      return res.redirect('/');
+    }
+    res.redirect('/signin');
+  };
 
-    // 連到 /admin 頁面就轉到 /admin/restaurants
-    app.get('/admin', (req, res) => res.redirect('/admin/restaurants'));
+  // 如果使用者訪問首頁，就導向 /restaurants 的頁面
+  app.get('/', authenticated, (req, res) => res.redirect('restaurants'));
+  app.get('/restaurants', authenticated, restController.getRestaurants);
 
-    // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
-    app.get('/admin/restaurants', adminController.getRestaurants);
+  // 連到 /admin 頁面就轉到 /admin/restaurants
+  app.get('/admin', authenticatedAdmin, (req, res) =>
+    res.redirect('/admin/restaurants')
+  );
+  app.get(
+    '/admin/restaurants',
+    authenticatedAdmin,
+    adminController.getRestaurants
+  );
 
-    app.get('/signin', userController.signInPage);
+  app.get('/signup', userController.signUpPage);
+  app.post('/signup', userController.signUp);
 
-    app.get('/signup', userController.signUpPage);
-
-    app.get('/logout', userController.logout);
-
-    app.post(
-        '/signin',
-        passport.authenticate('local', {
-            failureRedirect: '/signin',
-            failureFlash: true
-        }),
-        userController.signIn
-    );
-
-    app.post('/signup', userController.signUp);
+  app.get('/signin', userController.signInPage);
+  app.post(
+    '/signin',
+    passport.authenticate('local', {
+      failureRedirect: '/signin',
+      failureFlash: true
+    }),
+    userController.signIn
+  );
+  app.get('/logout', userController.logout);
 };
