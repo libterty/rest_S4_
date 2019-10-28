802ed813ab224bf12a0f2ee4ec19a4e9608f62e8
dreak603 <dreak603@gmail.com>
2019-10-28 16:58:33 +0800

    add signin and logout fn

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const flash = require('connect-flash');
+const passport = require('./config/passport');
 const session = require('express-session');
+const flash = require('connect-flash');
 const db = require('./models');
 const app = express();
 const port = 3000;
 
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+    require('dotenv').config();
 }
 
 // 設定 view engine 使用 handlebars
@@ -16,17 +17,18 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
 app.use(flash());
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages');
-  res.locals.error_messages = req.flash('error_messages');
-  next();
+    res.locals.success_messages = req.flash('success_messages');
+    res.locals.error_messages = req.flash('error_messages');
+    next();
 });
 
 app.listen(port, () => {
-  db.sequelize.sync();
-  console.log(`Example app listening on port http://localhost:${port}`);
+    db.sequelize.sync();
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
 
 require('./routes')(app);
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,15 @@ const db = require('../models');
 const User = db.User;
 
 const userController = {
+  signInPage: (req, res) => {
+    return res.render('signin');
+  },
+
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！');
+    res.redirect('/restaurants');
+  },
+
   signUpPage: (req, res) => {
     return res.render('signup');
   },
@@ -34,6 +43,12 @@ const userController = {
         }
       });
     }
+  },
+
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！');
+    req.logout();
+    res.redirect('/signin');
   }
 };
 
