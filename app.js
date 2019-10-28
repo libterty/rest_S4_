299ae9fc5f67e70cc9872790578689eb892ab298
dreak603 <dreak603@gmail.com>
2019-10-28 15:31:37 +0800

    add connect sql

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const db = require('./models');
 const app = express();
 const port = 3000;
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 // 設定 view engine 使用 handlebars
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+    db.sequelize.sync();
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
 
 require('./routes')(app);
