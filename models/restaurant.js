2d7fd3c02ede82c35cfc478cc3067228d6303957
dreak603 <dreak603@gmail.com>
2019-10-28 20:42:31 +0800

    add admin view

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,12 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Restaurant = sequelize.define('Restaurant', {
-    name: DataTypes.STRING,
-    tel: DataTypes.STRING,
-    address: DataTypes.STRING,
-    opening_hours: DataTypes.STRING,
-    description: DataTypes.TEXT
-  }, {});
+  const Restaurant = sequelize.define(
+    'Restaurant',
+    {
+      name: DataTypes.STRING,
+      tel: DataTypes.STRING,
+      address: DataTypes.STRING,
+      opening_hours: DataTypes.STRING,
+      description: DataTypes.TEXT
+    },
+    {}
+  );
   Restaurant.associate = function(models) {
     // associations can be defined here
   };
