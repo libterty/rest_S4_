1ef12c60204d48039daaf25e518e69df36c100c1
dreak603 <dreak603@gmail.com>
2019-10-29 01:57:10 +0800

    add prod port setup

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const db = require('./models');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
