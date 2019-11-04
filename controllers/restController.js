2c8352fa81f305f27f243571515dc25574534273
dreak603 <dreak603@gmail.com>
2019-11-04 15:37:47 +0800

    refactoring

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,59 +4,59 @@ const Category = db.Category;
 const pageLimit = 10;
 
 const restController = {
-    getRestaurants: (req, res) => {
-        let offset = 0;
-        let whereQuery = {};
-        let categoryId = '';
+  getRestaurants: (req, res) => {
+    let offset = 0;
+    let whereQuery = {};
+    let categoryId = '';
 
-        if (req.query.page) {
-            offset = (req.query.page - 1) * pageLimit;
-        }
-
-        if (req.query.categoryId) {
-            categoryId = Number(req.query.categoryId);
-            whereQuery['CategoryId'] = categoryId;
-        }
-
-        Restaurant.findAndCountAll({
-            include: Category,
-            where: whereQuery,
-            offset: offset,
-            limit: pageLimit
-        }).then(result => {
-            let page = Number(req.query.page) || 1;
-            let pages = Math.ceil(result.count / pageLimit);
-            let totalPage = Array.from({ length: pages }).map(
-                (item, index) => index + 1
-            );
-            let prev = page - 1 < 1 ? 1 : page - 1;
-            let next = page + 1 > pages ? pages : page + 1;
-            const data = result.rows.map(r => ({
-                ...r.dataValues,
-                description: r.dataValues.description.substring(0, 50)
-            }));
-            Category.findAll().then(categories => {
-                return res.render('restaurants', {
-                    restaurants: data,
-                    categories,
-                    categoryId,
-                    page,
-                    totalPage,
-                    prev,
-                    next
-                });
-            });
-        });
-    },
-
-    getRestaurant: (req, res) => {
-        return Restaurant.findByPk(req.params.id, {
-            include: Category
-        }).then(restaurant => {
-            return res.render('restaurant', {
-                restaurant
-            });
-        });
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit;
     }
+
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId);
+      whereQuery['CategoryId'] = categoryId;
+    }
+
+    Restaurant.findAndCountAll({
+      include: Category,
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
+    }).then(result => {
+      let page = Number(req.query.page) || 1;
+      let pages = Math.ceil(result.count / pageLimit);
+      let totalPage = Array.from({ length: pages }).map(
+        (item, index) => index + 1
+      );
+      let prev = page - 1 < 1 ? 1 : page - 1;
+      let next = page + 1 > pages ? pages : page + 1;
+      const data = result.rows.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50)
+      }));
+      Category.findAll().then(categories => {
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId,
+          page,
+          totalPage,
+          prev,
+          next
+        });
+      });
+    });
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    }).then(restaurant => {
+      return res.render('restaurant', {
+        restaurant
+      });
+    });
+  }
 };
 module.exports = restController;
