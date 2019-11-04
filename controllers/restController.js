67245ac3e37fe293f0d80d1701abaf89884914c4
dreak603 <dreak603@gmail.com>
2019-11-04 14:51:41 +0800

    create category options and controllers

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,13 +4,35 @@ const Category = db.Category;
 
 const restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r.dataValues,
-        description: r.dataValues.description.substring(0, 50)
-      }));
-      return res.render('restaurants', {
-        restaurants: data
+    let whereQuery = {};
+    let categoryId = '';
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId);
+      whereQuery['CategoryId'] = categoryId;
+    }
+    Restaurant.findAll({ include: Category, where: whereQuery }).then(
+      restaurants => {
+        const data = restaurants.map(r => ({
+          ...r.dataValues,
+          description: r.dataValues.description.substring(0, 50)
+        }));
+        Category.findAll().then(categories => {
+          return res.render('restaurants', {
+            restaurants: data,
+            categories,
+            categoryId
+          });
+        });
+      }
+    );
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    }).then(restaurant => {
+      return res.render('restaurant', {
+        restaurant
       });
     });
   }
